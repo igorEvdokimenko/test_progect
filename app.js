8bd0bccb7db3d95fcba3ebb287af353d18096886
master <rectangl28031992@gmail.com>
2021-08-12 13:28:27 +0300

    update 4

--- app.js
+++ app.js
@@ -70,10 +70,6 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-app.get('/', (req, res) => {
-    res.send('hello')
-})
-
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
